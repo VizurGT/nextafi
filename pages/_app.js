26aea5fab5bce87be78a4f29072d9d8388629d5a
VizurGT <78095160+VizurGT@users.noreply.github.com>
2025-06-05 01:02:05 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,50 @@
-// pages/_app.js
-import 'styles/globals.css'; // Importa tus estilos globales y de Tailwind
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+const GA_TRACKING_ID = "G-TTG6MZJVGB"; // Reemplaza con tu ID real
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag("config", GA_TRACKING_ID, {
+          page_path: url,
+        });
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
